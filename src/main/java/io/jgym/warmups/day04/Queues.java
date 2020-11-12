3889d4ee434fd050bb9b3a71d01000f44241733b
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-12 08:10:23 +0000

    The list of queues in the standard JDK

--- src/main/java/io/jgym/warmups/day04/Queues.java
+++ src/main/java/io/jgym/warmups/day04/Queues.java
@@ -5,5 +5,26 @@ import java.util.concurrent.*;
 
 public class Queues {
   public static void main(String... args) {
+    Queue<?> [] queues = {
+        // not threadsafe
+        new LinkedList<>(),
+        new ArrayDeque<>(),
+        new PriorityQueue<>(),
+
+        // threadsafe
+        new ConcurrentLinkedQueue<>(),
+        new ConcurrentLinkedDeque<>(),
+
+        new LinkedBlockingQueue<>(), // Fixed thread pool
+        new ArrayBlockingQueue<>(10),
+        new LinkedBlockingDeque<>(),
+
+        new LinkedTransferQueue<>(), // ForkJoinPool
+
+        // specialized
+        new SynchronousQueue<>(), // Cached thread pool
+        new DelayQueue<>(), // scheduled thread pool
+        new PriorityBlockingQueue<>(), // heap
+    };
   }
 }
