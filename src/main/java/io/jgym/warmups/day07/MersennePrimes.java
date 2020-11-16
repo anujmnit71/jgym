ae3d2404dfeff74cda4cada3607642208b92b7e1
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-16 21:08:53 +0200

    Day 07 - Getting ready for some Mersenne Primes

---
+++ src/main/java/io/jgym/warmups/day07/MersennePrimes.java
@@ -0,0 +1,7 @@
+package io.jgym.warmups.day07;
+
+// Heinz's ProjectEuler.net key: 717601_3I7Kn6n5LTCjvqEf4D5B27eL5lrWge0V
+public class MersennePrimes {
+    public static void main(String... args) {
+    }
+}
