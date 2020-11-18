b050d3257997946a0964a95b28c75abcccba3ee0
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-18 08:08:59 +0000

    Day 08 - Created a DaemonThreadFactory

--- src/main/java/io/jgym/warmups/day08/DaemonTimers.java
+++ src/main/java/io/jgym/warmups/day08/DaemonTimers.java
@@ -5,6 +5,18 @@ import java.util.concurrent.*;
 
 public class DaemonTimers {
     public static void main(String... args) throws InterruptedException {
+        Timer timer1 = new Timer(true);
+        timer1.schedule(new TimerTask() {
+            public void run() {
+                showDaemonStatus();
+            }
+        }, 1000, 1000);
+
+        var timer2 = Executors.newScheduledThreadPool(
+                1, new DaemonThreadFactory());
+        timer2.scheduleWithFixedDelay(DaemonTimers::showDaemonStatus,
+                1, 1, TimeUnit.SECONDS);
+        Thread.sleep(5500);
     }
 
     public static void showDaemonStatus() {
