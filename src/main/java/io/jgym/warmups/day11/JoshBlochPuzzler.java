3da746af735f373d0a4509ffdc3a2a5b6f46c38c
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-23 08:00:31 +0200

    Day 11 - Puzzles to start the week

---
+++ src/main/java/io/jgym/warmups/day11/JoshBlochPuzzler.java
@@ -0,0 +1,15 @@
+package io.jgym.warmups.day11;
+
+public class JoshBlochPuzzler {
+    // Puzzler: What does it print? Why?
+    // https://twitter.com/joshbloch/status/1330266449844310024
+    public static void main(String... args) {
+        short i = -1, j = 0; // short is 16 bits
+        while (i != 0) {
+            i = (short) ((i & 0xffff) >>> 1);  // >>> doesn't do sign extenstion
+            j++;
+        }
+        System.out.println(j);
+    }
+}
+// https://javaspecialists.teachable.com/p/bits-and-bytes
