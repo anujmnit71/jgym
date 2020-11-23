2bc4884ca4c530721ee200c1d3edd82538492bcc
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-23 08:27:23 +0200

    Day 11 - Puzzles to start the week

--- src/main/java/io/jgym/warmups/day11/JoshBlochPuzzler.java
+++ src/main/java/io/jgym/warmups/day11/JoshBlochPuzzler.java
@@ -6,7 +6,7 @@ public class JoshBlochPuzzler {
     public static void main(String... args) {
         short i = -1, j = 0; // short is 16 bits
         while (i != 0) {
-            i >>>= 1;  // >>> doesn't do sign extenstion
+            i >>>= 1;  // >>> doesn't do sign extension
             j++;
         }
         System.out.println(j);
