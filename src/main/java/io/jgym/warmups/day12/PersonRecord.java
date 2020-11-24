0a4644523e672c8d19b4e855b68036f0a2106437
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-24 08:16:46 +0000

    Day 12 serializing records - parameters the same order as PersonClass

--- src/main/java/io/jgym/warmups/day12/PersonRecord.java
+++ src/main/java/io/jgym/warmups/day12/PersonRecord.java
@@ -2,7 +2,7 @@ package io.jgym.warmups.day12;
 
 import java.io.*;
 
-public record PersonRecord(String lastName, String firstName, int age)
+public record PersonRecord(String firstName, String lastName, int age)
         implements Serializable {
     public PersonRecord {
         if (age < 0) throw new IllegalArgumentException("age < 0");
