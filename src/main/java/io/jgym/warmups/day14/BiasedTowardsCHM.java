93210bc0a57251141d890270c9ed9379e42f9e5b
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-26 08:02:01 +0000

    Day 14 in progress

--- src/main/java/io/jgym/warmups/day14/BiasedTowardsCHM.java
+++ src/main/java/io/jgym/warmups/day14/BiasedTowardsCHM.java
@@ -6,6 +6,20 @@ import java.util.stream.*;
 
 public class BiasedTowardsCHM {
     public static void main(String... args) {
-        System.out.println("Good morning JGym!");
+        for (int i = 0; i < 30; i++) {
+            test();
+        }
+    }
+
+    private static void test() {
+        Map<Long, Long> squares = new HashMap<>();
+        long time = System.nanoTime();
+        try {
+            LongStream.range(0, 1_000_000)
+                    .forEach(number -> squares.put(number * number, number));
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
     }
 }
