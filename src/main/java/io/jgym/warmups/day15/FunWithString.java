fe69817d4e46bd051d83e2d15e307a9d8ea05368
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-27 07:51:22 +0200

    Ready for day 15

---
+++ src/main/java/io/jgym/warmups/day15/FunWithString.java
@@ -0,0 +1,10 @@
+package io.jgym.warmups.day15;
+
+// From articles:
+//
+// https://www.javaspecialists.eu/archive/Issue277-Strings-with-Zero-HashCode.html
+// https://www.javaspecialists.eu/archive/Issue278-Free-Memory.html
+// https://horstmann.com/unblog/2020-04-02/
+
+public class FunWithString {
+}
