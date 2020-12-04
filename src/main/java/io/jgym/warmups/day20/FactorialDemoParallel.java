36b782a77aa8e5bcc5a06a10bd145f73f6bdf99f
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-12-04 08:03:25 +0000

    day 20 fun

---
+++ src/main/java/io/jgym/warmups/day20/FactorialDemoParallel.java
@@ -0,0 +1,45 @@
+package io.jgym.warmups.day20;
+
+import java.math.*;
+import java.util.concurrent.atomic.*;
+import java.util.stream.*;
+
+public class FactorialDemoParallel {
+    // https://www.javaspecialists.eu/courses/blackfriday20
+    private static LongAccumulator bestTime = new LongAccumulator(
+            Long::min, Long.MAX_VALUE
+    );
+    public static void main(String... args) {
+        for (int i = 0; i < 5; i++) {
+            test();
+        }
+        System.out.printf("%nbestTime = %dms%n", (bestTime.longValue() / 1_000_000));
+    }
+
+    private static void test() {
+        long time = System.nanoTime();
+        try {
+            BigInteger result = factorial(1_000_000);
+            System.out.println("result.bitCount() = " + result.bitCount());
+        } finally {
+            time = System.nanoTime() - time;
+            bestTime.accumulate(time);
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+    }
+
+    private static BigInteger factorial(int n) {
+        return factorial(0, n);
+    }
+
+    private static BigInteger factorial(int from, int to) {
+        if (from == to) {
+            if (from == 0) return BigInteger.ONE;
+            return BigInteger.valueOf(from);
+        }
+        int mid = (from + to) >>> 1;
+        var left = factorial(from, mid);
+        var right = factorial(mid + 1, to);
+        return left.multiply(right);
+    }
+}
