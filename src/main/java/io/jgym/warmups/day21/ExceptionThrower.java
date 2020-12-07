3c2ea09db897f250ced0c143b2e3b7a060c0c479
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-12-07 08:28:38 +0000

    Added method of sneaky throwing in Java 7

--- src/main/java/io/jgym/warmups/day21/ExceptionThrower.java
+++ src/main/java/io/jgym/warmups/day21/ExceptionThrower.java
@@ -3,18 +3,17 @@ package io.jgym.warmups.day21;
 import io.jgym.warmups.day08.*;
 import sun.misc.*;
 
-import java.io.*;
 import java.lang.reflect.*;
-import java.sql.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.*;
 
 public class ExceptionThrower {
     public static void rethrow(Throwable t) {
-//        sneakyThrow1(t);
+        sneakyThrow1(t);
+//        sneakyThrow1a(t);
 //        sneakyThrow2(t);
 //        sneakyThrow3(t);
-        sneakyThrow4(t);
+//        sneakyThrow4(t);
     }
 
     private static final ExecutorService thrower =
@@ -33,18 +32,24 @@ public class ExceptionThrower {
         LockSupport.park();
     }
 
+    private static void sneakyThrow1a(Throwable t) {
+        // Thread.currentThread().stop(t); // <--- this worked in Java 7
+    }
+
     private static final ThreadLocal<Throwable> toThrow = new ThreadLocal<>();
+
     private static class ThrowerClass {
         public ThrowerClass() throws Throwable {
             throw toThrow.get();
         }
     }
+
     private static void sneakyThrow2(Throwable t) {
         toThrow.set(t);
         try {
             ThrowerClass.class.newInstance();
-        } catch (ReflectiveOperationException ignore) { }
-        finally {
+        } catch (ReflectiveOperationException ignore) {
+        } finally {
             toThrow.remove();
         }
     }
@@ -68,10 +73,11 @@ public class ExceptionThrower {
      * limitations to evade compiler complaints about rethrowing
      * unchecked exceptions.
      */
-    @SuppressWarnings("unchecked") static <T extends Throwable>
+    @SuppressWarnings("unchecked")
+    static <T extends Throwable>
     void uncheckedThrow(Throwable t) throws T {
         if (t != null)
-            throw (T)t; // rely on vacuous cast
+            throw (T) t; // rely on vacuous cast
         else
             throw new Error("Unknown Exception");
     }
