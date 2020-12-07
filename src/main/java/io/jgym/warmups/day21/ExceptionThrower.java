cdd17e4cc4d2e4ff019df8d0bf187f6678a9e99e
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-12-07 08:01:36 +0200

    Ready for day 21

---
+++ src/main/java/io/jgym/warmups/day21/ExceptionThrower.java
@@ -0,0 +1,32 @@
+package io.jgym.warmups.day21;
+
+import sun.misc.*;
+
+import java.lang.reflect.*;
+import java.sql.*;
+import java.util.concurrent.locks.*;
+
+public class ExceptionThrower {
+    public static void rethrow(Throwable t) {
+        sneakyThrow1(t);
+//        sneakyThrow2(t);
+//        sneakyThrow3(t);
+//        sneakyThrow4(t);
+    }
+
+    private static void sneakyThrow1(Throwable t) {
+
+    }
+
+    private static void sneakyThrow2(Throwable t) {
+
+    }
+
+    private static void sneakyThrow3(Throwable t) {
+
+    }
+
+    private static void sneakyThrow4(Throwable t) {
+
+    }
+}
